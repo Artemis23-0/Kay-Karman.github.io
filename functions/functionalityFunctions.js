8437d43db8f80d4306b828a22c680953a064050c
Kay Karman <krystalj.karman@calbaptist.edu>
2023-04-07 23:02:03 -0700

    So much done

---
+++ functions/functionalityFunctions.js
@@ -0,0 +1,6 @@
+function sendEmail() {
+    var email = "krystal.j.karman@gmail.com";
+    var subject = "I found your portfolio! Let's Connect...";
+    var link = "mailto:" + email + "?subject=" + encodeURIComponent(subject);
+    window.open(link, "_blank");
+  }
