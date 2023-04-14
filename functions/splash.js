29471e87eebaa04ae1724c6dcf571c9a633991ab
Kay Karman <krystalj.karman@calbaptist.edu>
2023-04-14 10:11:10 -0700

    Optimizing Code

--- functions/splash.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-	var splash = document.querySelector('.splash');
-
-	// Delay the removal of the splash screen by 2 seconds
-	setTimeout(function() {
-		splash.style.display = 'none';
-		window.location.href = 'pages/home.html'; // Redirect to the main page
-	}, 2000);
-});
